50ed271aafa733f75fed0fab8eb0fc202086f9e0
yuxiu0729 <yuxiu0729@gmailcom>
2024-04-11 21:23:57 +0800

    Fweft Commit

---
+++ 0411/N09310015-1/N09310015-1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N09310015_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "請按我一下")
+                button1.Text = "我已經被按過了";
+
+            else if (button1.Text == "我已經被按過了")
+                button1.Text = "請按我一下";
+            label1.Text = "按鈕被按過了";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String count = label2.Text;
+            int sum = Int32.Parse(count) + 1;
+            label2.Text = sum.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String count = label2.Text;
+            int sum = Int32.Parse(count) - 1;
+            label2.Text = sum.ToString();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int a = Int32.Parse(textBox1.Text);
+            int b = Int32.Parse(textBox2.Text);
+            int sum = a + b;
+            label6.Text = sum.ToString();
+        }
+    }
+}
---
+++ 0411/N09310015/N09310015/Form1.cs
@@ -0,0 +1,10 @@
+namespace N09310015
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
